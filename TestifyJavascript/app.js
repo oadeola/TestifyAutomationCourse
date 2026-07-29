fe274dcb141141daddca82081d91af7b5fe90719
oadeola <adeolaoni.v@gmail.com>
2026-07-29 20:10:30 +0100

    Task 5 - Position of week concatenation

--- TestifyJavascript/app.js
+++ TestifyJavascript/app.js
@@ -1,1 +1,16 @@
-alert("Hello, World!");
+// const myFavoriteLanguage = 'Javascript'
+
+// const myAge = 25
+// const name = 'Adeola Oni'
+// const sentence = 'My name is ' + name + ' and I am ' + myAge + ' years old. My favorite programming language is ' + myFavoriteLanguage + '.'
+
+
+// console.log(typeof(myFavoriteLanguage))
+// console.log(typeof(myAge))
+// console.log(sentence)
+
+const day = 'Friday'
+const position = 5
+const output = 'Today is ' + day + '. It is the ' + position + 'th day of the week.'
+
+console.log(output)
