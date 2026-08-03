ea3cb6304678550d48daa601e08d436ccac31927
oadeola <adeolaoni.v@gmail.com>
2026-08-03 22:32:39 +0100

    add gitignore file

--- TestifyJavascript/app.js
+++ TestifyJavascript/app.js
@@ -1,16 +1,7 @@
-// const myFavoriteLanguage = 'Javascript'
+const number = 19;
 
-// const myAge = 25
-// const name = 'Adeola Oni'
-// const sentence = 'My name is ' + name + ' and I am ' + myAge + ' years old. My favorite programming language is ' + myFavoriteLanguage + '.'
-
-
-// console.log(typeof(myFavoriteLanguage))
-// console.log(typeof(myAge))
-// console.log(sentence)
-
-const day = 'Friday'
-const position = 5
-const output = 'Today is ' + day + '. It is the ' + position + 'th day of the week.'
-
-console.log(output)
+if (number % 2 === 0) {
+    console.log('The number is even.');
+} else {
+    console.log('The number is odd.');
+}
---
+++ TestifyJavascript/learn.js
@@ -0,0 +1,95 @@
+/*Concatenation learning starts here
+const myFavoriteLanguage = 'Javascript'
+
+const myAge = 25
+const name = 'Adeola Oni'
+const sentence = 'My name is ' + name + ' and I am ' + myAge + ' years old. My favorite programming language is ' + myFavoriteLanguage + '.'
+
+
+console.log(typeof(myFavoriteLanguage))
+console.log(typeof(myAge))
+console.log(sentence)
+
+
+const day = 'Friday'
+const position = 5
+const output = 'Today is ' + day + '. It is the ' + position + 'th day of the week.'
+
+console.log(output)
+
+
+Concatenation learning ends here*/
+
+/*Boolean learning starts here
+const isBritish = true
+const isNight = false
+const userDetails = null
+
+console.log(typeof(isBritish))
+console.log(typeof(isNight))
+console.log(typeof(userDetails))
+
+Boolean learning ends here*/
+
+/*Arithmetic learning starts here
+const addition = 5 + 10
+const subtraction = 10 - 5
+const multiplication = 5 * 10
+const division = 10 / 5
+const modulus = 40 % 6
+
+console.log('Addition: ' + addition)
+console.log('Subtraction: ' + subtraction)
+console.log('Multiplication: ' + multiplication)
+console.log('Division: ' + division)
+console.log('Modulus: ' + modulus)
+
+Arithmetic learning ends here*/
+
+/*Comparison learning starts here
+const comparison1 = 10 > 5
+const comparison2 = 10 < 5
+const comparison3 = 10 >= 5
+const comparison4 = 10 <= 5
+const comparison5 = 10 == 5
+const comparison6 = 10 != 5
+const comparison7 = 10 === (5*2)
+
+console.log('Comparison 1 (10 > 5): ' + comparison1)
+console.log('Comparison 2 (10 < 5): ' + comparison2)
+console.log('Comparison 3 (10 >= 5): ' + comparison3)
+console.log('Comparison 4 (10 <= 5): ' + comparison4)
+console.log('Comparison 5 (10 == 5): ' + comparison5)
+console.log('Comparison 6 (10 != 5): ' + comparison6)
+console.log('Comparison 7 (10 === 10): ' + comparison7)
+
+Comparison learning ends here*/
+
+/*Logic learning starts here
+const userName = 'Adeola'
+const userAge = 32
+
+const logic1 = (10 > 5) && (5 < 10)
+const logic2 = (10 > 5) || (5 > 10)
+const logic3 = !(10 > 5)
+const logic4 = (userName === 'Adeola') && (userAge >= 18)
+
+console.log('Logic 1 (10 > 5 && 5 < 10): ' + logic1)
+console.log('Logic 2 (10 > 5 || 5 > 10): ' + logic2)
+console.log('Logic 3 (!(10 > 5)): ' + logic3)
+console.log('Logic 4 (userName === "Adeola" && userAge >= 18): ' + logic4)
+console.log('Logic 5 (More Logic): ' + (userName === 'Vicky' || userAge < 18))
+
+Logic learning ends here*/
+
+/*Conditional learning starts here
+const userAge = 15
+
+if (userAge >= 18) {
+    console.log('You are an adult.')
+} else {
+    console.log('You are not an adult.')
+}
+
+Conditional learning ends here*/
+
