94b17ebfa16eb9573156c78c15d4d5eb5fe93cbd
oadeola <adeolaoni.v@gmail.com>
2026-08-04 23:00:01 +0100

    Task 9 - Area of Rectangle

--- TestifyJavascript/app.js
+++ TestifyJavascript/app.js
@@ -1,16 +1,6 @@
-// for (let star = 10; star <=100; star++) {
-//     if(star ===1){
-//          console.log(star + ' star')
-//     }
-//     else {
-//     console.log(star + ' stars')
-//     }
-// }
-
-
-for (let number = 1; number <= 20; number = number + 1) {
-    if (number % 2 === 1) {
-        console.log(number + ' is an odd number')
-    }
-
+function calculateAreaOfRectangle(length, width) {
+    const area = length * width;
+    console.log(area);
 }
+
+calculateAreaOfRectangle(4, 12);
