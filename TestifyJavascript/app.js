efb65a5ffe3cbed84d5523e9d6de66ba2b8399e1
oadeola <adeolaoni.v@gmail.com>
2026-08-07 09:55:34 +0100

    Task 12 - Pick your Best Friend

--- TestifyJavascript/app.js
+++ TestifyJavascript/app.js
@@ -1,4 +1,7 @@
-//Task 11
 const myFriends = ['Sharon', 'Benjamin', 'Emily', 'Bookie', 'Frank'];
 
 console.log(myFriends);
+
+//Task12 - Pick your best Friend.
+const bestFriend = myFriends[0];
+console.log('My best friend is ' + bestFriend);
