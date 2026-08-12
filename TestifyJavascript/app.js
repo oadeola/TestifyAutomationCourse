1ef36da60c5141a8674ad54e0e815952c1c5f212
oadeola <adeolaoni.v@gmail.com>
2026-08-12 20:10:32 +0100

    Task 15 - My Personal Library 3

--- TestifyJavascript/app.js
+++ TestifyJavascript/app.js
@@ -1,25 +1,45 @@
+//Task 15 - My Personal Library 3
 
-//Task 14 - My Personal Library 2
+const books = [
+    {
+        title: "The Temptation",
+        description: "A novel about love and betrayal",
+        numberOfPages: 255,
+        author: "Colleen Hoover",
+        readingStatus: false,
+   },
+     {
+        title: "Child of the Morning",
+        description: "A novel about love and betrayal",
+        numberOfPages: 255,
+        author: "Adetomi Adeyemi",
+        readingStatus: true,
+   },
+     {
+        title: "The Miracle Morning",
+        description: "A novel about peace and happiness",
+        numberOfPages: 485,
+        author: "Sue Knight",
+        readingStatus: false,
+   },
+     {
+        title: "Mermaid's chair",
+        description: "A novel about faith and love",
+        numberOfPages: 1025,
+        author: "Ben Harris",
+        readingStatus: true,
+   },
+     {
+        title: "Middle Daughter",
+        description: "Grief, love, and family",
+        numberOfPages: 322,
+        author: "Chika Unigwe",
+        readingStatus: false,
+   },
 
-const books = {
-    title: "The Temptation",
-    description: "A novel about love and betrayal",
-    numberOfPages: 255,
-    author: "Colleen Hoover",
-    readingStatus: false,
-    toggleReadingStatus: function(){
-       if (books.readingStatus === true) {
-           books.readingStatus = false;
-       } else {
-           books.readingStatus = true;
-       }
+]
+for (let i = 0; i < books.length; i++) {
+    if (books[i].readingStatus === true) {
+        console.log(books[i].title + " (" + books[i].description + ")" + " by " + books[i].author + " with " + books[i].numberOfPages + " pages")
     }
 }
-books.toggleReadingStatus();
-
-console.log(books.title)
-console.log(books.description)
-console.log(books.numberOfPages)
-console.log(books['author'])
-console.log(books['readingStatus'])
-
