c0c7d3fdf15aa6fc555dd70bbfb912752208b691
oadeola <adeolaoni.v@gmail.com>
2026-08-14 22:48:00 +0100

    Challenge 5 - Create a function that reverses an array

---
+++ TestifyJavascript/challenge5 - Reversed array.js
@@ -0,0 +1,9 @@
+// Challenge 5 - Create a function that reverses an array
+
+function reverseArray(array) {
+    return array.reverse();
+}
+const numbers = [1, 2, 3, 4, 5];
+
+console.log('[1, 2, 3, 4, 5] in reverse is: ' + reverseArray(numbers));
+
