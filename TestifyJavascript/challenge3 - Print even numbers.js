0603b72b72cd263cf5cac3897ad0abb47e715d3f
oadeola <adeolaoni.v@gmail.com>
2026-08-14 22:45:36 +0100

    Challenge 3 - Print all even numbers from 0 – 100

---
+++ TestifyJavascript/challenge3 - Print even numbers.js
@@ -0,0 +1,6 @@
+//Challenge 3: Print all even numbers from 0 – 100
+for (let number = 0; number <= 100; number++) {
+  if (number % 2 === 0) {
+    console.log(number + " is an even number");
+  }
+}
