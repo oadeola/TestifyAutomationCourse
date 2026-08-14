f63fc2379364095dd1f0eed50378c4d2b1e64e5b
oadeola <adeolaoni.v@gmail.com>
2026-08-14 22:49:05 +0100

    Challenge 6 - Sort an array of strings in alphabetical order

---
+++ TestifyJavascript/challenge6 - Sort array alphabetically.js
@@ -0,0 +1,8 @@
+//Challenge 6 - Sort an array of strings in alphabetical order.
+function sortAlphabetically(array) {
+    return array.sort();
+}
+
+const colors = ["Purple", "Green", "Yellow", "White", "Aqua"];
+
+console.log(sortAlphabetically(colors));
