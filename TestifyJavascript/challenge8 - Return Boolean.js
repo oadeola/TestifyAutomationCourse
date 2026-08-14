6638df25925f3e3c5269b6f158d7e32319eb7a13
oadeola <adeolaoni.v@gmail.com>
2026-08-14 22:55:11 +0100

    Challenge 8 - Return a Boolean if a number is divisible by 10

---
+++ TestifyJavascript/challenge8 - Return Boolean.js
@@ -0,0 +1,8 @@
+//Challenge 8 - Return a Boolean if a number is divisible by 10.
+
+for (i = 1; i <= 100; i++){
+    if (i % 10 === 0){
+        console.log("True - " + i + " is divisible by 10")
+    }
+}
+    
