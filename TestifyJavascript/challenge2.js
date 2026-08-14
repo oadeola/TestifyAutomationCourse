ab6d68de862e509141e62d25cef5151b989a2d1d
oadeola <adeolaoni.v@gmail.com>
2026-08-14 23:00:04 +0100

    file rename

--- TestifyJavascript/challenge2.js
+++
@@ -1,8 +0,0 @@
-//Challenge 2: Create a length converter function that converts feet to inches, yards, and miles
-function lengthConverter(feet) {
-  const inches = feet * 12;
-  const yards = feet / 3;
-  const miles = feet / 5280;
-  return { inches, yards, miles };
-}
-console.log(lengthConverter(12)); 
