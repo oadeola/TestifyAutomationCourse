7b4fbcb44caa5ea6422e02e9d0a4925523f644c9
oadeola <adeolaoni.v@gmail.com>
2026-09-02 20:29:09 +0100

    Task 1 - Print out Several Lines

---
+++ Module4C_Java/Tasks/Task1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!, Let's do Java!");
+        System.out.println("Here's to learning and mastering Java!");
+        System.out.println("Testify Academy is great.");
+        System.out.println("Looking forward to being a fantastic Test Automation Engineer.");
+        System.out.println("I've got this!!!");
+    }
+}
+
