38bda143976cd48dfeb3c0c5410ddb7e27d1eb06
oadeola <adeolaoni.v@gmail.com>
2026-09-02 21:03:46 +0100

    Task 2 - Print Datatype Variables

---
+++ Module4C_Java/FirstJavaProject/src/JavaDataTypes.java
@@ -0,0 +1,18 @@
+public class JavaDataTypes {
+    static void main(String[] args) {
+
+        byte age = 115;
+        short shortMoney = -31_555;
+        long longMoney = 999911122;
+        int intMoney = -909991234;
+        float area1 = 499222.11f;
+        double area2 = 12344444.5622;
+
+        System.out.println("Age: "  + age);
+        System.out.println("ShortMoney: "  + shortMoney);
+        System.out.println("LongMoney: "  + longMoney);
+        System.out.println("IntMoney: "  + intMoney);
+        System.out.println("Area1: "  + area1);
+        System.out.println("Area2: "  + area2);
+    }
+}
