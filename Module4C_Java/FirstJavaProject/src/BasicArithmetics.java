4332e80de977d9ddd9259f8e39a29895a7e2ff51
oadeola <adeolaoni.v@gmail.com>
2026-09-07 19:41:05 +0100

    Task 4 - Area Of A Circle

---
+++ Module4C_Java/FirstJavaProject/src/BasicArithmetics.java
@@ -0,0 +1,21 @@
+public class BasicArithmetics {
+    static void main(String[] args){
+        //Basic Arithmetics =,-,*,/
+          double age1 = 40;
+          double age2 = 20;
+
+
+        //System.out.println(age1%age2);
+
+        //incremental and decremental
+
+       //double agePlus = age1 ++;
+       // System.out.println(agePlus);
+
+
+        //compound assignment operator
+        age1 -= age2;
+        System.out.println(age1);
+
+    }
+}
---
+++ Module4C_Java/Tasks/Task4/src/AreaOfACircle.java
@@ -0,0 +1,12 @@
+//Task 4 - Area Of A Circle
+public class AreaOfACircle {
+    static void main(String[] args){
+        double pi = 3.14;
+        double radius1 = 12.7;
+        double radius2 = 12.7;
+
+        //area of a circle
+        double area = (pi * radius1 * radius2);
+        System.out.println("Area of a circle is: " + area +"meters");
+    }
+}
